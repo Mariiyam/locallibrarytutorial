56a2c6b7a4df3479589fa5e4aa6ff6d255e43bbd
Mariam Abdalla <mariam3455.mk@gmail.com>
2022-03-28 17:46:26 +0200

    added models, an index page to display counts of records of each model, and that includes url mapping a url i chose to be the endpoint for the view i created that renders a template i created with the data from all the models, the template is html practiced with it the template extend, and all that and that

---
+++ locallibrary/catalog/views.py
@@ -0,0 +1,25 @@
+from unicodedata import name
+from django.shortcuts import render
+from .models import Book, BookInstance, Author, Genre
+
+# Create your views here.
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact= 'a').count
+    num_authors = Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_engineering =  Book.objects.filter(title__iexact= 'engineering').count()
+    
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors' : num_authors,
+        'num_genres' : num_genres,
+        'num_books_engineering': num_books_engineering,
+    }
+    
+    
+    return render(request, 'index.html', context= context)
